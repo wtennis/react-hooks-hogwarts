784916a0b19592a6de9e40414ef3a364dad63d54
Whiting <whitinghtennis@gmail.com>
2021-07-23 16:27:56 -0700

    grease button

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -1,20 +1,38 @@
 import HogCard from "./HogCard";
-
+import {useState} from "react";
 
 function Hoglist({hogs}) {
     
-
-
     const hogList = hogs.map((hogObj) => {
         return (
             <HogCard key = {hogObj.name} hog = {hogObj}/>
         )
     })
 
+    const [showGreased, setShowGreased] = useState(false)
+
+    // Define greasedList using hogList.filter()
+    const greasedListArr = hogs.filter((hog)=> hog.greased ===true)
+
+    const greasedList = greasedListArr.map((hogObj) => {
+        return (
+            <HogCard key = {hogObj.name} hog = {hogObj}/>
+        )
+    })
+
+    function toggleGreased() {
+        setShowGreased(!showGreased)
+    }
+
     return (
-        <div className = "ui grid container">
-        {hogList}
-          </div>
+        <>
+		<button onClick= {toggleGreased}>Greased/Ungreased</button>
+        <hr></hr>
+            <div className = "ui grid container">
+                {/* ternary to show hogList or greasedList based on showGreased useState */}
+                {showGreased ? greasedList : hogList}
+            </div>
+          </>
 
     )
 }
